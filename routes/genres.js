1080b5f28f77af0861676648f62b11ff136f36d4
Matej Stremfelj <matej.stremfelj26@gmail.com>
2024-08-13 18:06:50 +0200

    Adding persistence to Genres API

--- routes/genres.js
+++ routes/genres.js
@@ -1,53 +1,71 @@
 const express = require("express");
 const genresRouter = express.Router();
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Comedy" },
-  { id: 4, name: "Romance" },
-  { id: 5, name: "Thriller" },
-  { id: 6, name: "Drama" },
-  { id: 7, name: "Sci-Fi" },
-  { id: 8, name: "Fantasy" },
-  { id: 9, name: "Mystery" },
-  { id: 10, name: "Adventure" },
-  { id: 11, name: "Crime" },
-  { id: 12, name: "Animation" }
-];
+// Connecting to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Vidly")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
-genresRouter.get("/", (req, res) => {
-  res.send(genres);
+// Creating a genre Schema
+const genreSchema = new mongoose.Schema({
+  name: String
 });
 
-genresRouter.post("/", (req, res) => {
+// Creating a genre model
+const Genre = mongoose.model("Genre", genreSchema);
+
+// Fetching all genres
+genresRouter.get("/", async (req, res) => {
+  try {
+    const genres = await Genre.find();
+    res.send(genres);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// Posting a genre
+genresRouter.post("/", async (req, res) => {
   const schema = Joi.object({
-    genre: Joi.string().min(3).max(20).required()
+    name: Joi.string().min(3).max(20).required()
   });
   const result = schema.validate(req.body);
   if (result.error) {
     res.status(400).send(result.error.details[0].message);
     return;
   }
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.genre
-  };
-  genres.push(genre);
-  res.send(genre);
-});
+  let genre = new Genre({ name: req.body.name }).save();
 
-genresRouter.get("/:id", (req, res) => {
-  const genre = genres.find((g) => {
-    return g.id === parseInt(req.params.id);
-  });
-  if (!genre) {
-    res.status(404).send("The genre with the given ID was not found");
-    return;
+  try {
+    genre = await genre.save();
+    res.send(genre);
+  } catch (err) {
+    res.status(400).send(err.message);
   }
-  res.send(genre);
 });
 
+// Displaying a genre by ID
+genresRouter.get("/:id", async (req, res) => {
+  try {
+    const genre = await Genre.findById(req.params.id);
+    if (!genre) {
+      res.send("The genre with the given ID was not found");
+      return;
+    }
+    res.send(genre);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// Updating a genre
 genresRouter.put("/:id", (req, res) => {
   const genre = genres.find((g) => g.id === parseInt(req.params.id));
   if (!genre) {
